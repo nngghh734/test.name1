77c3c77d31d31d51a94815821626475e6da85117
nngghh734 <166725073+nngghh734@users.noreply.github.com>
2024-04-11 17:19:24 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+def quarter(x, y):
+    if x > 0 and y > 0:
+        print('I')
+    if x > 0 and y < 0:
+        print('IV')
+    if x < 0 and y > 0:
+        print('II')
+    if x < 0 and y < 0:
+        print('III')
+ 
+ 
+quarter(-3, -3)
